c6d08b5d764ea0e18110e62004c83a4a1fb3ed90
zhujinrun <15997352948@163.com>
2021-08-20 15:06:51 +0800

    Windows 字符集的介绍

--- mfc/mfc.cpp
+++ mfc/mfc.cpp
@@ -84,4 +84,30 @@ void MyFrame::OnPaint()
 	dc.Ellipse(10, 10, 100, 100);
 }
 
+// Windows 字符集
+// ANSI            多字节：1字节/字符
+// 中文 韩文...    宽字节：1个字符对应多个字节 Unicode utf8 3字节/字符 gbk 2字节/字符
 
+// 多字节 转 宽字节 的几种方式
+// MessageBox(L"aaaaaaa");
+// MessageBox(TEXT("aaaaaa")); // TEXT、TCHAR 自动编码转换
+
+// 统计宽字节字符串的长度
+// char* p = "aaaa";
+// int n = 0;
+// n = strlen(p);
+//
+// wchar_t * p2 = L"bbbbb";
+// n = wcslen(p2);
+
+// string与char* char[]之间的转换 https://www.cnblogs.com/cs0915/p/13060525.html
+
+// CString 与 char* 之间的转换
+// char* p3 = "aa";
+// CString str = CString(p3);
+//
+// CString str2 = TEXT("abc");
+// CStringA tmp;
+// tmp = str2;
+// char* pp = tmp.GetBuffer();
+// MessageBox(CString(pp));
